dfa48e3952428d3a11ebd8ad86196c1322e25968
Aliaksandr Adamovich <nice.sasha.03@gmail.com>
2024-11-28 00:27:59 +0100

    Login and registration
    - Login interface has been updated
    - Added the initial design of the registration page
    - The initial version of the logo has been added

--- edupoint-frontend/src/components/LoginPage.tsx
+++ edupoint-frontend/src/components/LoginPage.tsx
@@ -1,47 +1,142 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { login } from '../redux/authSlice';
-import { useNavigate } from 'react-router-dom';
-import { Button, TextField, Typography } from '@mui/material';
+import {useDispatch} from 'react-redux';
+import {login} from '../redux/authSlice';
+import {useNavigate} from 'react-router-dom';
+import {Button, TextField, Typography, Box} from '@mui/material';
 import styled from 'styled-components';
 
-const LoginContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
+const PageContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vw;
+    background-color: #f3f3f3;
+    box-sizing: border-box;
+    padding: 20px;
+    margin: 0;
+`;
+
+
+const FormContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 40px 30px;
+    background: #fff;
+    border-radius: 12px;
+    box-shadow: 0px 8px 20px rgba(0, 0, 0, 0.1);
+    max-width: 500px;
+    width: 100%;
+`;
+
+const LogoContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 30px;
+    width: 100%;
+
+    img {
+        width: 48px;
+        height: 48px;
+        margin-right: 10px;
+    }
+
+    h1 {
+        font-size: 24px;
+        font-weight: bold;
+        color: #333;
+    }
+`;
+
+const ForgotPassword = styled.a`
+    font-size: 14px;
+    color: #888;
+    margin-top: 10px;
+    text-decoration: none;
+    cursor: pointer;
+    align-self: flex-start;
+
+    &:hover {
+        color: #555;
+        text-decoration: underline;
+    }
 `;
 
 const LoginPage: React.FC = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-  const handleLogin = () => {
-    dispatch(
-      login({
-        id: '12345',
-        name: 'John',
-        surname: 'Doe',
-        jwt: 'mock-jwt-token',
-        profilePicture: 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541'
-      })
+    const handleLogin = () => {
+        dispatch(
+            login({
+                id: '12345',
+                name: 'John',
+                surname: 'Doe',
+                jwt: 'mock-jwt-token',
+                profilePicture:
+                    'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541',
+            })
+        );
+        navigate('/dashboard');
+    };
+
+    return (
+        <PageContainer>
+            <FormContainer>
+
+                <LogoContainer>
+                    <img src="/icons/logo.png" alt="EduPoint Logo"/>
+                    <h1>EduPoint</h1>
+                </LogoContainer>
+
+
+                <Typography variant="h4" gutterBottom style={{fontWeight: 'bold', marginBottom: '20px'}}>
+                    Sign in
+                </Typography>
+
+
+                <TextField
+                    label="E-mail"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    InputProps={{
+                        style: {borderRadius: '8px'},
+                    }}
+                />
+                <TextField
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    InputProps={{
+                        style: {borderRadius: '8px'},
+                    }}
+                />
+
+                <ForgotPassword href="#">Forgot password?</ForgotPassword>
+
+                <Button
+                    variant="contained"
+                    style={{
+                        backgroundColor: '#7c4dff',
+                        color: '#fff',
+                        marginTop: '20px',
+                        padding: '10px 20px',
+                        textTransform: 'none',
+                        fontWeight: 'bold',
+                        borderRadius: '8px',
+                    }}
+                    fullWidth
+                    onClick={handleLogin}
+                >
+                    Login
+                </Button>
+            </FormContainer>
+        </PageContainer>
     );
-    navigate('/dashboard');
-  };
-
-  return (
-    <LoginContainer>
-      <Typography variant="h4" gutterBottom>
-        Login
-      </Typography>
-      <TextField label="Username" variant="outlined" margin="normal" />
-      <TextField label="Password" type="password" variant="outlined" margin="normal" />
-      <Button variant="contained" color="primary" onClick={handleLogin}>
-        Login
-      </Button>
-    </LoginContainer>
-  );
 };
 
 export default LoginPage;
--- edupoint-frontend/src/components/RegisterPage.tsx
+++ edupoint-frontend/src/components/RegisterPage.tsx
@@ -1,13 +1,161 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import {Button, TextField, Typography, styled as muiStyled} from '@mui/material';
+import styled from 'styled-components';
+import {login} from '../redux/authSlice';
+import {useDispatch} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+
+const PageContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vw;
+    background-color: #f3f3f3;
+    padding: 20px;
+    box-sizing: border-box;
+`;
+
+const FormContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 40px 30px;
+    background: #fff;
+    border-radius: 12px;
+    box-shadow: 0px 8px 20px rgba(0, 0, 0, 0.1);
+    max-width: 500px;
+    width: 100%;
+`;
+
+const LogoContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 30px;
+    width: 100%;
+
+    img {
+        width: 48px;
+        height: 48px;
+        margin-right: 10px;
+    }
+
+    h1 {
+        font-size: 24px;
+        font-weight: bold;
+        color: #333;
+    }
+`;
+
+const CustomTextField = muiStyled(TextField)({
+    '& .MuiOutlinedInput-root': {
+        backgroundColor: '#f8f8f8',
+        borderRadius: '8px',
+        fontFamily: 'Roboto, sans-serif',
+        fontWeight: 700,
+        color: '#333',
+        '& input': {
+            fontWeight: 700,
+        },
+        '& fieldset': {
+            borderColor: '#e0e0e0',
+        },
+        '&:hover fieldset': {
+            borderColor: '#bdbdbd',
+        },
+        '&.Mui-focused fieldset': {
+            borderColor: '#7c4dff',
+        },
+    },
+    '& .MuiInputLabel-root': {
+        color: '#888',
+        fontSize: '14px',
+    },
+    '& .MuiInputLabel-root.Mui-focused': {
+        color: '#7c4dff',
+    },
+});
+
 
 const RegisterPage: React.FC = () => {
-  return (
-    <div>
-      <Typography variant="h4">Register</Typography>
-      <p>Mocked Registration Page</p>
-    </div>
-  );
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleRegister = () => {
+        dispatch(
+            login({
+                id: '12345',
+                name: 'John',
+                surname: 'Doe',
+                jwt: 'mock-jwt-token',
+                profilePicture:
+                    'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541',
+            })
+        );
+        navigate('/dashboard');
+    };
+
+    return (
+        <PageContainer>
+            <FormContainer>
+                <LogoContainer>
+                    <img src="/icons/logo.png" alt="EduPoint Logo"/>
+                    <h1>EduPoint</h1>
+                </LogoContainer>
+
+
+                <Typography variant="h4" gutterBottom style={{fontWeight: 'bold', marginBottom: '20px'}}>
+                    Sign up
+                </Typography>
+
+
+                <CustomTextField
+                    label="Username"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                />
+                <CustomTextField
+                    label="E-mail"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                />
+                <CustomTextField
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                />
+                <CustomTextField
+                    label="Confirm Password"
+                    type="password"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                />
+
+            
+                <Button
+                    variant="contained"
+                    style={{
+                        backgroundColor: '#7c4dff',
+                        color: '#fff',
+                        marginTop: '20px',
+                        padding: '10px 20px',
+                        textTransform: 'none',
+                        fontWeight: 'bold',
+                        borderRadius: '8px',
+                    }}
+                    fullWidth
+                    onClick={handleRegister}
+                >
+                    Sign up
+                </Button>
+            </FormContainer>
+        </PageContainer>
+    );
 };
 
 export default RegisterPage;
